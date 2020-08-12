a4deea186e36b056f085608256d2c7f0f0a20e76
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-08-12 13:36:59 +0530

    chore: setup eslint and prettier
    1. Add new addons and recently scored addons list
    2. Get data from emberobserver api

---
+++ README.md
@@ -0,0 +1,16 @@
+#ember-observer-cli
+A CLI for [emberobserver.com](https://emberobserver.com), so now you can install your ember addons from the command-line
+
+
+## Install
+```
+npm install -g ember-observer-cli
+```
+
+## Usage
+```
+ember-observer-cli
+```
+
+
+
--- index.js
+++ index.js
@@ -1,162 +1,216 @@
-const blessed = require("blessed");
+const blessed = require('blessed');
+const fetch = require('node-fetch');
 
 const screen = blessed.screen({ fullUnicode: true });
 
 const header = blessed.box({
   parent: screen,
-  content: "Ember Observer CLI",
+  content: 'Ember Observer CLI',
   top: 0,
   left: 0,
-  width: "100%",
-  height: "10%",
+  width: '100%',
+  height: '10%',
   border: {
-    type: "line",
-    fg: "white",
+    type: 'line',
+    fg: 'white',
   },
 });
 
-const listItems = ["{red-fg}Apple{/red-fg}", "Orange", "Banana", "Grapes"];
+const categoriesUrl =
+  'https://emberobserver.com/api/v2/categories?include=subcategories,parent';
+
+const topAddonsUrl =
+  'https://emberobserver.com/api/v2/addons?filter[top]=true&include=categories&page[limit]=10&sort=ranking';
+
+const rsaUrl =
+  'https://emberobserver.com/api/v2/addons?filter[recentlyReviewed]=true&include=categories&page[limit]=10';
+
+const newAddonsUrl =
+  'https://emberobserver.com/api/v2/addons?include=categories&page[limit]=10&sort=-publishedDate';
 
 const navbar = blessed.list({
   parent: screen,
-  top: "10%+1",
+  top: '10%+1',
   left: 0,
-  width: "30%",
-  height: "90%",
+  width: '30%',
+  height: '90%',
   border: {
-    type: "line",
-    fg: "white",
+    type: 'line',
+    fg: 'white',
   },
-  label: "Categories",
-  items: listItems,
+  label: 'Categories',
   keys: true,
   vi: true,
   style: {
     selected: {
-      fg: "black",
-      bg: "white",
+      fg: 'black',
+      bg: 'white',
     },
     focus: {
       border: {
-        fg: "yellow",
+        fg: 'yellow',
       },
     },
   },
   tags: true,
 });
 
-const topAddons = [
-  {
-    name: "ember-cli-babel",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-cli-typescript",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-resolver",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-concurrency",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "qunit-dom",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-data",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-fetch",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-cli-mirage",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-basic-dropdown",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-  {
-    name: "ember-composable-helpers",
-    description: "Ember CLI addon for Babel",
-    category: "Build Tools",
-    lastUpdated: "11 days ago",
-  },
-];
-const topAddonsList = topAddons.map((a, index) => {
-  let str = "{red-fg}#" + (index + 1) + "{/red-fg} ";
-  str += "{yellow-fg}{bold}" + a.name + "{/} ";
-  str += a.description + "\r\n";
-  str += a.category + " " + "Last Updated " + a.lastUpdated;
-  return str;
-});
-const content = blessed.list({
+fetch(categoriesUrl)
+  .then((res) => res.json())
+  .then((json) => {
+    const items = json.data.map((j) => {
+      const { name, 'addon-count': addonCount } = j.attributes;
+      return `${name} (${addonCount})`;
+    });
+    navbar.setItems(items);
+    screen.render();
+  });
+
+const topAddonsList = blessed.list({
   parent: screen,
-  top: "10%+1",
-  left: "30%+1",
-  width: "70%",
-  height: "90%",
+  top: '10%+1',
+  left: '30%+1',
+  width: '70%',
+  height: '30%',
   border: {
-    type: "line",
-    fg: "white",
+    type: 'line',
+    fg: 'white',
   },
   style: {
     selected: {
-      fg: "black",
-      bg: "white",
+      fg: 'black',
+      bg: 'white',
     },
     focus: {
       border: {
-        fg: "yellow",
+        fg: 'yellow',
       },
     },
   },
-  label: "Top Addons",
-  items: topAddonsList,
+  label: 'Top Addons',
   keys: true,
   vi: true,
   tags: true,
 });
 
-screen.key(["q"], () => {
-  return process.exit(0);
+fetch(topAddonsUrl)
+  .then((res) => res.json())
+  .then((json) => {
+    const items = json.data.map((a, index) => {
+      const { name, description, 'updated-at': updatedAt } = a.attributes;
+      let str = '{red-fg}#' + (index + 1) + '{/red-fg} ';
+      str += '{yellow-fg}{bold}' + name + '{/} ';
+      str += description;
+      str += 'unknown' + ' ' + 'Last Updated ' + updatedAt;
+      return str;
+    });
+    topAddonsList.setItems(items);
+    screen.render();
+  });
+
+// Recently Scored Addons List
+const rsaList = blessed.list({
+  parent: screen,
+  top: '40%+1',
+  left: '30%+1',
+  width: '70%',
+  height: '30%',
+  border: {
+    type: 'line',
+    fg: 'white',
+  },
+  style: {
+    selected: {
+      fg: 'black',
+      bg: 'white',
+    },
+    focus: {
+      border: {
+        fg: 'yellow',
+      },
+    },
+  },
+  label: 'Recently Scored Addons',
+  keys: true,
+  vi: true,
+  tags: true,
 });
 
-navbar.key("tab", () => {
-  content.focus();
+fetch(rsaUrl)
+  .then((res) => res.json())
+  .then((json) => {
+    const items = json.data.map((a, index) => {
+      const { name, description, 'updated-at': updatedAt } = a.attributes;
+      let str = '{red-fg}#' + (index + 1) + '{/red-fg} ';
+      str += '{yellow-fg}{bold}' + name + '{/} ';
+      str += description;
+      str += 'unknown' + ' ' + 'Last Updated ' + updatedAt;
+      return str;
+    });
+    rsaList.setItems(items);
+    screen.render();
+  });
+
+// New  Addons List
+const newList = blessed.list({
+  parent: screen,
+  top: '70%+1',
+  left: '30%+1',
+  width: '70%',
+  height: '30%',
+  border: {
+    type: 'line',
+    fg: 'white',
+  },
+  style: {
+    selected: {
+      fg: 'black',
+      bg: 'white',
+    },
+    focus: {
+      border: {
+        fg: 'yellow',
+      },
+    },
+  },
+  label: 'Recently Scored Addons',
+  keys: true,
+  vi: true,
+  tags: true,
 });
 
-content.key("tab", () => {
+fetch(newAddonsUrl)
+  .then((res) => res.json())
+  .then((json) => {
+    const items = json.data.map((a, index) => {
+      const { name, description, 'updated-at': updatedAt } = a.attributes;
+      let str = '{red-fg}#' + (index + 1) + '{/red-fg} ';
+      str += '{yellow-fg}{bold}' + name + '{/} ';
+      str += description;
+      str += 'unknown' + ' ' + 'Last Updated ' + updatedAt;
+      return str;
+    });
+    newList.setItems(items);
+    screen.render();
+  });
+
+screen.key(['q'], () => {
+  return process.exit(0); // eslint-disable-line
+});
+
+navbar.key('tab', () => {
+  topAddonsList.focus();
+});
+
+topAddonsList.key('tab', () => {
   navbar.focus();
 });
 
 screen.append(header);
 screen.append(navbar);
-screen.append(content);
+screen.append(topAddonsList);
+screen.append(rsaList);
+screen.append(newList);
 
 navbar.focus();
 
